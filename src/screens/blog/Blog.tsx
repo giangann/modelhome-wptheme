3f2dc70cb5b374e8f3a0934a680e471f5d2b4477
giangann <70595717+giangann@users.noreply.github.com>
2023-04-02 03:53:30 +0700

    fix: ts

--- src/screens/blog/Blog.tsx
+++ src/screens/blog/Blog.tsx
@@ -8,20 +8,25 @@ import { BlogSidebar } from './BlogSidebar';
 
 export const Blog = () => {
   const { data: listBlog, isLoading: isLoading } = useQuery<BlogApiType[]>('blogs');
-  return isLoading ? (
-    'Loading'
-  ) : (
-    <Container sx={{ mt: 8 }}>
-      <Grid container columnSpacing={4}>
-        <Grid container item xs={12} sm={7}>
-          {listBlog?.map((blog, index) => (
-            <Grid key={index} item xs={12}>
-              <BlogItem blog={blog} />
+  return (
+    <>
+      {isLoading ? (
+        // put a placeholder of logo or beauty animation here
+        'Loading'
+      ) : (
+        <Container sx={{ mt: 8 }}>
+          <Grid container columnSpacing={4}>
+            <Grid container item xs={12} sm={7}>
+              {listBlog?.map((blog, index) => (
+                <Grid key={index} item xs={12}>
+                  <BlogItem blog={blog} />
+                </Grid>
+              ))}
             </Grid>
-          ))}
-        </Grid>
-        <BlogSidebar />
-      </Grid>
-    </Container>
+            <BlogSidebar />
+          </Grid>
+        </Container>
+      )}
+    </>
   );
 };
